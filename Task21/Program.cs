ced16f86b452703e6dff36cb0a510a1eef0130f0
DariaMuravitskaya <muraviczkaya96@gmail.com>
2022-11-21 12:21:40 +0300

    added folder 21

---
+++ Task21/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53*/
+
+Console.WriteLine(decimal])
+
+/*Console.Write("Введите Y координату 1й точки: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Z координату 1й точки");
+int z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите Х координату 2й точки");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Y координату 2й точки");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Z координату 1й точки");
+int z2 = Convert.ToInt32(Console.ReadLine()); */
+
+
+
