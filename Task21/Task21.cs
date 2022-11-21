30ad5ce608a85458f8f7e3feb54bb223efd6a8c2
DariaMuravitskaya <muraviczkaya96@gmail.com>
2022-11-21 12:53:09 +0300

    solved task21 var2

--- Task21/Task21.cs
+++ Task21/Task21.cs
@@ -3,7 +3,7 @@ A (3,6,8); B (2,1,-7), -> 15.84
 A (7,-5, 0); B (1,-1,9) -> 11.53*/
 
 
-Console.Write("Введите X координату 1й точки: ");
+/* Console.Write("Введите X координату 1й точки: ");
 double x1 = Convert.ToDouble(Console.ReadLine());
 Console.Write("Введите Y координату 1й точки: ");
 double y1 = Convert.ToDouble(Console.ReadLine());
@@ -18,7 +18,30 @@ Console.Write("Введите Z координату 1й точки: ");
 double z2 = Convert.ToDouble(Console.ReadLine()); 
 
 double result = DisPoint(x1, y1, z1, x2, y2, z2);
+Console.WriteLine($"{result}"); 
+
+static double DisPoint(double x1, double y1, double z1, double x2, double y2, double z2)
+{
+    return Math.Sqrt(Math.Pow(x2-x1, 2) + Math.Pow(y2-y1, 2) + Math.Pow(z2-z1, 2));
+}
+*/
+
+double x1 = UserInput("Введите координату X1: ");
+double y1 = UserInput("Введите координату Y1: ");
+double z1 = UserInput("Введите координату Z1: ");
+double x2 = UserInput("Введите координату X2: ");
+double y2 = UserInput("Введите координату Y2: ");
+double z2 = UserInput("Введите координату Z2: ");
+
+
+double result = DisPoint(x1, y1, z1, x2, y2, z2);
 Console.WriteLine($"{result}");
+
+static double UserInput (string message)
+{
+    Console.Write(message);
+    return Convert.ToDouble(Console.ReadLine()); 
+}
 
 static double DisPoint(double x1, double y1, double z1, double x2, double y2, double z2)
 {
